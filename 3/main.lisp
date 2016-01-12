f0ff19b2f883282bb364258c01c916629d28a13e
TatriX <tatrics@gmail.com>
2016-01-12 14:07:59 +0300

    Added third tutorial

---
+++ 3/main.lisp
@@ -0,0 +1,26 @@
+(defpackage #:sdl-tutorial-3
+  (:use :common-lisp :sdl2)
+  (:export :c-main :main))
+
+(in-package :sdl-tutorial-3)
+
+(defparameter *screen-width* 640)
+(defparameter *screen-height* 480)
+
+(defmacro with-window-and-renderer (window renderer &body body)
+  `(sdl2:with-init (:video)
+    (sdl2:with-window (,window :title "SDL2 Tutorial" :w *screen-width* :h *screen-height*)
+      (sdl2:with-renderer (,renderer ,window)
+        ,@body))))
+
+(defun main()
+  (with-window-and-renderer window renderer
+    (let* ((image (sdl2:load-bmp "exit.bmp"))
+           (texture (sdl2:create-texture-from-surface renderer image)))
+      (sdl2:with-event-loop (:method :poll)
+        (:quit () t)
+        (:idle ()
+         (sdl2:render-copy renderer texture)
+         (sdl2:render-present renderer)))
+      (sdl2:destroy-texture texture)
+      (sdl2:free-surface image))))
