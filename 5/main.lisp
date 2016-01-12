7e534e335d8157b5cf88c52266e295a2e7bf48a8
TatriX <tatrics@gmail.com>
2016-01-12 20:22:43 +0300

    Removed renderer from 5

--- 5/main.lisp
+++ 5/main.lisp
@@ -1,5 +1,5 @@
 (defpackage #:sdl-tutorial-5
-  (:use :common-lisp :sdl2)
+  (:use :common-lisp)
   (:export :main))
 
 (in-package :sdl-tutorial-5)
@@ -7,23 +7,27 @@
 (defparameter *screen-width* 640)
 (defparameter *screen-height* 480)
 
-(defmacro with-window-and-renderer (window renderer &body body)
+(defmacro with-window (window &body body)
   `(sdl2:with-init (:video)
-     (sdl2:with-window (,window :title "SDL2 Tutorial" :w *screen-width* :h *screen-height*)
-       (sdl2:with-renderer (,renderer ,window)
-         ,@body))))
+     (sdl2:with-window (,window
+                        :title "SDL2 Tutorial"
+                        :w *screen-width*
+                        :h *screen-height*
+                        :flags '(:shown))
+         ,@body)))
 
-(defun load-texture (window renderer filename)
-  (sdl2:create-texture-from-surface renderer
-                                    (sdl2:convert-surface-format (sdl2:load-bmp filename)
-                                                                 (sdl2:surface-format-format
-                                                                  (sdl2:get-window-surface window)))))
+(defun load-surface (filename &optional pixel-format)
+  (sdl2:convert-surface-format (sdl2:load-bmp filename) pixel-format))
 
 (defun main()
-  (with-window-and-renderer window renderer
-    (let ((texture (load-texture window renderer "stretch.bmp")))
+  (with-window window
+    (let* ((window-surface (sdl2:get-window-surface window))
+           (image-surface (load-surface "stretch.bmp" (sdl2:surface-format-format window-surface))))
       (sdl2:with-event-loop (:method :poll)
         (:quit () t)
         (:idle ()
-               (sdl2:render-copy renderer texture)
-               (sdl2:render-present renderer))))))
+               (sdl2:blit-scaled image-surface
+                                 nil
+                                 window-surface
+                                 (sdl2:make-rect 0 0 *screen-width* *screen-height*))
+               (sdl2:update-window window))))))
