27eaafaed38d88bd8ef25707f6dcf89575db2f60
TatriX <tatrics@gmail.com>
2016-01-13 23:50:44 +0300

    Added sixth tutorial

---
+++ 6/main.lisp
@@ -0,0 +1,33 @@
+(defpackage #:sdl-tutorial-6
+  (:use :common-lisp)
+  (:export :main))
+
+(in-package :sdl-tutorial-6)
+
+(defparameter *screen-width* 640)
+(defparameter *screen-height* 480)
+
+(defmacro with-window-surface (window surface &body body)
+  `(sdl2:with-init (:video)
+     (sdl2:with-window (,window
+                        :title "SDL2 Tutorial"
+                        :w *screen-width*
+                        :h *screen-height*
+                        :flags '(:shown))
+       (let ((,surface (sdl2:get-window-surface ,window)))
+         ,@body))))
+
+(defun load-surface (filename &optional pixel-format)
+  (sdl2:convert-surface-format (sdl2-image:load-image filename) pixel-format))
+
+(defun main()
+  (with-window-surface window screen-surface
+    (sdl2-image:init '(:png))
+    (sdl2:with-event-loop (:method :poll)
+      (:quit () t)
+      (:idle ()
+             (sdl2:blit-scaled (load-surface "6/loaded.png" (sdl2:surface-format-format screen-surface))
+                               nil
+                               screen-surface
+                               (sdl2:make-rect 0 0 *screen-width* *screen-height*))
+             (sdl2:update-window window)))))
