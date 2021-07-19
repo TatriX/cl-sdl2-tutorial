8efaa281657343ec166f645bd861577be1dd8237
TatriX <tatrics@gmail.com>
2021-07-19 23:42:54 +0200

    Refactor tutorial 07

--- 07-texture-loading-and-rendering.lisp
+++ 07-texture-loading-and-rendering.lisp
@@ -1,8 +1,8 @@
-(defpackage #:sdl2-tutorial-7
-  (:use :common-lisp)
-  (:export :main))
+(defpackage #:sdl2-tutorial-07-texture-loading-and-rendering
+  (:use :cl)
+  (:export :run))
 
-(in-package :sdl2-tutorial-7)
+(in-package :sdl2-tutorial-07-texture-loading-and-rendering)
 
 (defparameter *screen-width* 640)
 (defparameter *screen-height* 480)
@@ -10,21 +10,22 @@
 (defmacro with-window-renderer ((window renderer) &body body)
   `(sdl2:with-init (:video)
      (sdl2:with-window (,window
-                        :title "SDL2 Tutorial"
+                        :title "SDL2 Tutorial 07"
                         :w *screen-width*
                         :h *screen-height*
                         :flags '(:shown))
        (sdl2:with-renderer (,renderer ,window :index -1 :flags '(:accelerated))
-                           ,@body))))
+         ,@body))))
 
-(defun load-texture (renderer filename)
-  (sdl2:create-texture-from-surface renderer (sdl2-image:load-image filename)))
+(defun load-texture (renderer pathname)
+  (let ((fullpath (merge-pathnames pathname (asdf:system-source-directory :sdl2-tutorial))))
+    (sdl2:create-texture-from-surface renderer (sdl2-image:load-image fullpath))))
 
-(defun main()
+(defun run()
   (with-window-renderer (window renderer)
     (sdl2-image:init '(:png))
     (sdl2:set-render-draw-color renderer #xFF #xFF #xFF #xFF)
-    (let ((texture (load-texture renderer "7/texture.png")))
+    (let ((texture (load-texture renderer "assets/07/texture.png")))
       (sdl2:with-event-loop (:method :poll)
         (:quit () t)
         (:idle ()
